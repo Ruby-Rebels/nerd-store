1c0601c2acce0d99a77b626e36941881b26264d1
JackMarx <seriousfools@gmail.com>
2016-05-14 23:57:08 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get '/' => 'novelties#index'
+  get '/novelties' => 'novelties#index'
+  get '/novelties/new' => 'novelties#new'
+  post '/novelties' => 'novelties#create'
+  get '/novelties/:id' => 'novelties#show'
+  get '/novelties/:id/edit' => 'novelties#edit'
+  patch '/novelties/:id' => 'novelties#update'
+  delete '/novelties/:id' => 'novelties#delete'
+end
