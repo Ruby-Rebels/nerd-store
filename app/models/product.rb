3d686de5816613b790b429833d673ffec0a7fc9c
Trevor Jones <trevordavidjones@gmail.com>
2016-07-27 09:59:52 -0400

    added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_i < 2
+      "Discount Item!"
+    else
+      "Not on Sale!"
+    end
+  end
+
+  def tax
+    price.to_i * 0.09
+  end
+
+  def total
+    tax + price.to_i
+  end
 end
