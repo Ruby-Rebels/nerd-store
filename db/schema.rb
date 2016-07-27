44edb6ccd91e8d257d12719c81b57de6a66f5096
Trevor Jones <trevordavidjones@gmail.com>
2016-07-27 10:33:01 -0400

    Update database and styles
    Change price datatype to decimal
    Add a bit of styling to buttons

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515040737) do
+ActiveRecord::Schema.define(version: 20160727142239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "price"
+    t.decimal  "price"
     t.string   "description"
     t.string   "image"
     t.datetime "created_at",  null: false
