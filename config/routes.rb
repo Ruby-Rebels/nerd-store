31a4b1c67f9629844a843b1ba7b3b5321be17fd4
Trevor Jones <trevordavidjones@gmail.com>
2016-08-01 08:45:34 -0400

    added User and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   get '/images/new' => 'images#new'
   post '/images' => 'images#create'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
